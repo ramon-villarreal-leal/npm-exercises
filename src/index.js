c2337fdefaa2ec7c796e98e9952452ef40f04db5
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-05-26 17:58:54 -0500

    updating exercise done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const sayHello = () => console.log("hello");
+"use strict";
+
+const $ = require('jquery');
+
+var randomColor = require('randomcolor'); // import the script
+var color = randomColor(); // a hex code for an attractive color
+var color2 = randomColor(); // a hex code for an attractive color
+
+
+const sayHello = () => console.log('Meow Meow');
+
+
 
 sayHello();
+
+$('body').css('background-color', color);
+$('h1').css('color', color2);
+
